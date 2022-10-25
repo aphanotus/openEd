4331932b78fef73a7a3d833132886ec918aebc55
Dave Angelini <drangeli@colby.edu>
2022-10-25 12:37:44 -0400

    updated code

--- BI377.22F.morphometry/class6.Oct25/class.code.221025.R
+++ BI377.22F.morphometry/class6.Oct25/class.code.221025.R
@@ -139,6 +139,70 @@ shape.space(pca.pupfish, group = sex.pop, convex.hulls = TRUE,
             backtransform.examples = TRUE,
             ref.shape = gpa.pupfish$consensus)
 
+#####
+# B. borealis v. B. fervidus?
+#####
+
+# Borrowing code from last week!
+# If you already have the GPA objects in your R environment, then
+# you may not need to re-run these lines
+fw.links <- matrix(c(1,2, 1,5, 5,4, 4,3, 3,2, 5,6, 6,7, 7,8, 8,9, 
+                     9,4, 3,11, 11,12, 11,10, 9,10, 10,14, 14,15, 15,16, 
+                     16,18, 18,20, 16,17, 17,8, 12,13, 13,19, 14,13, 18,19, 
+                     2,12),
+                   ncol = 2, byrow = TRUE)
+(fw.links <- as.matrix(read.csv("fw.links.csv")))
+hw.links <- matrix(c(1:5,2:6), ncol = 2, byrow = FALSE)
+
+xy.fw <- read.tps("bi377.demo.borealis.v.fervidus.forewings.class.tps", keep.original.ids = TRUE)
+xy.hw <- read.tps("bi377.demo.borealis.v.fervidus.hindwings.class.tps", keep.original.ids = TRUE)
+xy.fw <- align.reflect(xy.fw, top.pt = 2, left.pt = 19, links = fw.links, show.plot = 15)
+xy.hw <- align.reflect(xy.hw, top.pt = 1, left.pt = 6, links = hw.links, show.plot = 15)
+# Data curation
+xy.fw$coords[7:10,,61] <- xy.fw$coords[8:11,,61]
+xy.fw$coords[11,,61] <- NA
+xy.fw <- add.provenance(
+  xy.fw,
+  name="error.correction",
+  title = "Corrected error in the placement of landmarks 7-11",
+  text = "The digitizer placed landmark 7 twice and shifted the position of landmarks 7-10. No data exist for landmark 11, which was set to NA." )
+xy.fw <- estimate.missing.landmarks(xy.fw)
+gpa.fw <- align.procrustes(xy.fw)
+tmp <- xy.hw$coords[2,,45]
+xy.hw$coords[2,,45] <- xy.hw$coords[3,,45]
+xy.hw$coords[3,,45] <- tmp
+xy.hw <- add.provenance(
+  xy.hw,
+  name="error.correction",
+  title = "Corrected error in the placement of landmarks 2&3",
+  text = "The digitizer swapped landmarks 2 and 3. This error has been corrected" )
+gpa.hw <- align.procrustes(xy.hw)
+
+# Blinded metadata!
+blinded.metadata <- read.csv("bi377.demo.borealis.v.fervidus.metadata.csv")
+blinded.metadata
+
+
+# Forewing PCA
+pca.fw <- gm.prcomp(gpa.fw$gdf$coords)
+pcvar(pca.fw)
+
+shape.space(pca.fw, group = blinded.metadata$species, convex.hulls = TRUE, 
+            include.legend = TRUE, group.title = "species")
+
+species <- rep(blinded.metadata$species,8)
+
+shape.space(pca.fw, group = species, convex.hulls = TRUE, 
+            include.legend = TRUE, group.title = "species")
+
+shape.space(pca.fw, group = species, convex.hulls = TRUE, 
+            include.legend = TRUE, group.title = "species",
+            axis1 = 1, axis2 = 3)
+
+shape.space(pca.fw, group = species, convex.hulls = TRUE, 
+            include.legend = TRUE, group.title = "species",
+            backtransform.examples = TRUE,
+            ref.shape = gpa.fw$consensus)
 
 
 
