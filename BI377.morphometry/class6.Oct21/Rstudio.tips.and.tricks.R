ecd94705fa83a7b3e082bccb338bd4502c79abac
Dave Angelini <drangeli@colby.edu>
2024-10-25 10:46:47 -0400

    Rstudio.tips.and.tricks.R

---
+++ BI377.morphometry/class6.Oct21/Rstudio.tips.and.tricks.R
@@ -0,0 +1,31 @@
+# Rstudio tips and tricks
+
+# Auto completion for functions
+hea
+
+# Auto completion for arguments
+landmark.plot(plethodon, spe)
+
+# Auto wrapping of quotes, parentheses, and `backticks`
+landmark.plot(plethodon, text.color = blue)
+
+# Comand-C to copy selection
+# Comand-X to cut (copy and delete) selection
+# Command-V to paste selection
+# Command-Z to undo an edit
+
+# Shift-Command-C to comment a line
+# rm(list=ls())
+
+# Command-D to delete a line
+
+# Shift-Command-D to duplicate a line -- or duplicate a selection within or across lines
+
+# In Rstudio don't use Shift-Command-S for "Save-As"!
+# Rstudio default is that Shift-Command-S is "Source"
+# which runs all code in the active document
+
+# Instead "Save-As" is Option-Command-S
+
+
+# Happy coding!
